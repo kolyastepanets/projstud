99efe001e5a38192ed3dfd8184d57b02284f54bf
kolyastepanets <kolyastepanets@gmail.com>
2015-06-02 13:59:39 +0300

    test for models: orders, length, dependent

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
-	has_many :answers
+	has_many :answers, -> { order('created_at DESC') }, dependent: :destroy
 	validates :title, :body, presence: true
+	validates :title, length: { maximum: 50 }
 end
