9c0e477ea95d869360165fb03ca7fa25c42a4f9e
kolyastepanets <kolyastepanets@gmail.com>
2015-06-02 16:03:56 +0300

    remaking task

--- db/migrate/20150601193627_create_answers.rb
+++ db/migrate/20150601193627_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :body
+      t.integer :question_id
 
       t.timestamps null: false
     end
