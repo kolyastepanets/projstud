086b900fdc2112bbedad04a3c78721551c7b0cfb
kolyastepanets <kolyastepanets@gmail.com>
2015-06-02 13:59:39 +0300

    test for models: orders, length, dependent

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-	it { should have_many(:answers) }
+	it { should have_many(:answers).order('created_at DESC').dependent(:destroy) }
 	it { should validate_presence_of :title }
 	it { should validate_presence_of :body }
-
+	it { should validate_length_of(:title).is_at_most(50) }
 end
